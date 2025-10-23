4f065f59692d91199c7cf65775ef25ae71096445
lina_aziz <lenaexo0@gmail.com>
2025-10-23 13:28:29 +0300

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -5,3 +5,6 @@
  * @tree: node from which to measure backwards, starting at 0
  * Return: levels from root, or 0 if `tree` is NULL
  */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
