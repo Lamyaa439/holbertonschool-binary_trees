a6310d2b453d27e486b71d62af328e2cfb691057
lina_aziz <lenaexo0@gmail.com>
2025-10-23 13:32:09 +0300

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,10 +9,12 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
-if (!tree)
+
+	if (!tree)
 		return (0);
+
 	for (depth = 0; tree->parent; depth++)
-	tree = tree->parent;
+		tree = tree->parent;
 
 	return (depth);
 }
