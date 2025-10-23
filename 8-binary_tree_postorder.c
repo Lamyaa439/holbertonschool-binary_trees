afad069f8f71332d107ab466823325429b32ffa7
lina_aziz <lenaexo0@gmail.com>
2025-10-23 13:39:37 +0300

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_postorder - recursively proceeds through a `binary_tree_t`
  * binary tree using post-order traversal
@@ -10,4 +11,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
+
 	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
