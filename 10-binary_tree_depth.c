01a9eba815f20d6d0754de0a4dd1408c24401dcb
lina_aziz <lenaexo0@gmail.com>
2025-10-23 13:28:08 +0300

    Create 10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,7 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: node from which to measure backwards, starting at 0
+ * Return: levels from root, or 0 if `tree` is NULL
+ */
