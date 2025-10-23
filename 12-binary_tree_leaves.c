7d83e1b8a116fc21e2256cf37b139d3b0872787d
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-23 06:39:22 +0000

    update 12-binary_tree_leaves.c file

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,5 +10,16 @@
  **/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t count;
 
+	count = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL)
+		count += 1;
+	if (tree->right != NULL)
+		count += 1;
+
+	return (count);
 }
