d2b345dacbe8650fa78a59307137bb29da5ea2e0
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-26 10:20:53 +0000

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,14 +11,11 @@
  **/
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *left = NULL;
-	binary_tree_t *right = NULL;
-
 	if (!node || !node->parent)
 		return (NULL);
 
 	if (node == node->parent->left)
 		return (node->parent->right);
 	else
-		return (node->parent->left)
+		return (node->parent->left);
 }
