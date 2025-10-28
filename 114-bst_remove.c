f3d99c49f92483d8f2cd44adc2663a36ee41ad8a
lina_aziz <lenaexo0@gmail.com>
2025-10-28 14:10:16 +0300

    Update 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
 /**
+ * min_value_node - finds the node with minimum value in a subtree
+ * @node: pointer to the subtree root
+ *
+ * Return: pointer to the node with minimum value
+ */
+bst_t *min_value_node(bst_t *node)
+{
+	bst_t *current = node;
+
+	while (current && current->left)
+		current = current->left;
+
+	return (current);
+}
+
+/**
  * bst_remove - removes a node from a Binary Search Tree
  * @root: pointer to the root node of the tree
  * @value: value to remove in the tree
@@ -9,52 +25,37 @@
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *tmp, *successor;
+	bst_t *temp, *successor;
 
 	if (root == NULL)
 		return (NULL);
 
 	if (value < root->n)
-	{
 		root->left = bst_remove(root->left, value);
-		if (root->left)
-			root->left->parent = root;
-	}
 	else if (value > root->n)
-	{
 		root->right = bst_remove(root->right, value);
-		if (root->right)
-			root->right->parent = root;
-	}
 	else
 	{
-		/* Node with only one child or no child */
 		if (root->left == NULL)
 		{
-			tmp = root->right;
-			if (tmp)
-				tmp->parent = root->parent;
+			temp = root->right;
+			if (temp)
+				temp->parent = root->parent;
 			free(root);
-			return (tmp);
+			return (temp);
 		}
 		else if (root->right == NULL)
 		{
-			tmp = root->left;
-			if (tmp)
-				tmp->parent = root->parent;
+			temp = root->left;
+			if (temp)
+				temp->parent = root->parent;
 			free(root);
-			return (tmp);
+			return (temp);
 		}
 
-		/* Node with two children: get inorder successor */
-		successor = root->right;
-		while (successor->left)
-			successor = successor->left;
-
+		successor = min_value_node(root->right);
 		root->n = successor->n;
 		root->right = bst_remove(root->right, successor->n);
-		if (root->right)
-			root->right->parent = root;
 	}
 
 	return (root);
