f7bfaea6f9478e6c250a60aa09a7714dd4a21258
lina_aziz <lenaexo0@gmail.com>
2025-10-28 14:13:14 +0300

    Update 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
  * min_value_node - finds the node with minimum value in a subtree
@@ -6,7 +7,7 @@
  *
  * Return: pointer to the node with minimum value
  */
-bst_t *min_value_node(bst_t *node)
+static bst_t *min_value_node(bst_t *node)
 {
 	bst_t *current = node;
 
@@ -25,37 +26,47 @@ bst_t *min_value_node(bst_t *node)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *temp, *successor;
+	bst_t *tmp, *successor;
 
 	if (root == NULL)
 		return (NULL);
 
 	if (value < root->n)
+	{
 		root->left = bst_remove(root->left, value);
+		if (root->left)
+			root->left->parent = root;
+	}
 	else if (value > root->n)
+	{
 		root->right = bst_remove(root->right, value);
+		if (root->right)
+			root->right->parent = root;
+	}
 	else
 	{
 		if (root->left == NULL)
 		{
-			temp = root->right;
-			if (temp)
-				temp->parent = root->parent;
+			tmp = root->right;
+			if (tmp)
+				tmp->parent = root->parent;
 			free(root);
-			return (temp);
+			return (tmp);
 		}
 		else if (root->right == NULL)
 		{
-			temp = root->left;
-			if (temp)
-				temp->parent = root->parent;
+			tmp = root->left;
+			if (tmp)
+				tmp->parent = root->parent;
 			free(root);
-			return (temp);
+			return (tmp);
 		}
 
 		successor = min_value_node(root->right);
 		root->n = successor->n;
 		root->right = bst_remove(root->right, successor->n);
+		if (root->right)
+			root->right->parent = root;
 	}
 
 	return (root);
