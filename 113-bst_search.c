d128760234b8dc691d7787c283ed61cf05cc7dc1
lina_aziz <lenaexo0@gmail.com>
2025-10-28 15:02:25 +0300

    Update 113-bst_search.c

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -12,10 +12,11 @@ bst_t *bst_search(const bst_t *tree, int value)
 	if (tree == NULL)
 		return (NULL);
 
-	if (tree->n == value)
+	if (value == tree->n)
 		return ((bst_t *)tree);
-	else if (value < tree->n)
+
+	if (value < tree->n)
 		return (bst_search(tree->left, value));
-	else
-		return (bst_search(tree->right, value));
+
+	return (bst_search(tree->right, value));
 }
