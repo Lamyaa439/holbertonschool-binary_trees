893fe6f5342c45360c5fe0bdd12406ed3a45bc6f
lina_aziz <lenaexo0@gmail.com>
2025-10-28 14:31:04 +0300

    Update 122-array_to_avl.c

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,55 +1,50 @@
 #include "binary_trees.h"
 
 /**
- * avl_insert_rec - helper to insert a value into an AVL tree
- * @tree: double pointer to the root node
- * @array: pointer to the array
- * @start: starting index
- * @end: ending index
+ * avl_insert - inserts a value in an AVL Tree
+ * @tree: double pointer to the root node of the AVL tree
+ * @value: value to insert
  *
- * Return: pointer to the root node
+ * Return: pointer to the created node, or NULL on failure
  */
-static avl_t *avl_insert_rec(avl_t **tree, int *array, int start, int end)
+avl_t *avl_insert(avl_t **tree, int value)
 {
-	int mid;
-	avl_t *node;
+	avl_t *node = NULL;
 
-	if (start > end)
+	if (tree == NULL)
 		return (NULL);
-
-	mid = (start + end) / 2;
-	node = binary_tree_node(NULL, array[mid]);
-	if (node == NULL)
-		return (NULL);
-
-	node->left = avl_insert_rec(&node->left, array, start, mid - 1);
-	if (node->left)
-		node->left->parent = node;
-
-	node->right = avl_insert_rec(&node->right, array, mid + 1, end);
-	if (node->right)
-		node->right->parent = node;
 
 	if (*tree == NULL)
-		*tree = node;
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
 
-	return (node);
-}
-
-/**
- * array_to_avl - builds an AVL tree from an array
- * @array: pointer to the first element of the array
- * @size: number of elements in the array
- *
- * Return: pointer to the root node of the created AVL tree
- */
-avl_t *array_to_avl(int *array, size_t size)
-{
-	avl_t *root = NULL;
-
-	if (array == NULL || size == 0)
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			node = binary_tree_node(*tree, value);
+			(*tree)->left = node;
+		}
+		else
+			node = avl_insert(&((*tree)->left), value);
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			node = binary_tree_node(*tree, value);
+			(*tree)->right = node;
+		}
+		else
+			node = avl_insert(&((*tree)->right), value);
+	}
+	else
 		return (NULL);
 
-	avl_insert_rec(&root, array, 0, (int)size - 1);
-	return (root);
+	/* Fix balance */
+	binary_tree_balance(*tree);
+
+	return (node);
 }
