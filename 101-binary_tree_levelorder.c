e92f85606694defd0a41593cac4d55f183a49cce
lina_aziz <lenaexo0@gmail.com>
2025-10-28 14:53:33 +0300

    Update 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - goes through a binary tree
+ *                          using level-order traversal
  * @tree: pointer to the root node of the tree
  * @func: pointer to a function to call for each node
  */
-void binary_tree_levelorder(const binary_tree_t *tree,
-			    void (*func)(int))
+void binary_tree_levelorder(
+	const binary_tree_t *tree, void (*func)(int))
 {
 	const binary_tree_t **queue;
 	size_t front = 0, rear = 0, size = 0;
