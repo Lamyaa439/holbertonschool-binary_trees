60d8e2155025bbf2ede9c556b74f8625c8606551
lina_aziz <lenaexo0@gmail.com>
2025-10-30 11:08:31 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -82,5 +82,6 @@ heap_t *heap_extract(heap_t **root);
 heap_t *heap_insert(heap_t **root, int value);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
 
 #endif /* _BINARY_TREES_H_ */
