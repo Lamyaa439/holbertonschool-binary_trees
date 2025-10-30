975b5d4822a9338e4f432fdef5b38886271f62eb
lina_aziz <lenaexo0@gmail.com>
2025-10-30 13:05:34 +0300

    Update 134-heap_to_sorted_array.c

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -3,42 +3,30 @@
 
 /**
  * heap_to_sorted_array - converts a Binary Max Heap to a sorted array
- * @heap: pointer to heap root
- * @size: pointer to store the array size
- * Return: pointer to sorted array, or NULL on failure
+ * @heap: pointer to root node of the heap
+ * @size: address to store size of array
+ *
+ * Return: pointer to sorted array (descending order), or NULL
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
+	size_t i = 0, heap_size;
 	int *array;
-	size_t count = 0, i = 0;
-	heap_t *tmp = heap;
 
 	if (!heap || !size)
 		return (NULL);
 
-	/* count nodes to allocate array */
-	while (tmp)
-	{
-		tmp = tmp->right;
-		count++;
-	}
+	heap_size = binary_tree_size(heap);
+	*size = heap_size;
 
-	array = malloc(sizeof(int) * count);
+	array = malloc(sizeof(int) * heap_size);
 	if (!array)
 		return (NULL);
 
-	for (i = 0; i < count; i++)
-		array[i] = heap_extract(&heap);
-
-	/* reverse array to make it ascending order */
-	for (i = 0; i < count / 2; i++)
+	while (heap)
 	{
-		int tmp_val = array[i];
-
-		array[i] = array[count - 1 - i];
-		array[count - 1 - i] = tmp_val;
+		array[i++] = heap_extract(&heap);
 	}
 
-	*size = count;
 	return (array);
 }
