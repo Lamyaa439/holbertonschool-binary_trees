8ff6feb32fb00b7a16a14fe198a019422f77bb40
lina_aziz <lenaexo0@gmail.com>
2025-10-30 12:23:17 +0300

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,106 +1,71 @@
 #include "binary_trees.h"
 
 /**
- * heap_size - measures the size of a binary tree
- * @root: pointer to the root node
- *
- * Return: size of tree
+ * swap_values - swaps values of two nodes
+ * @a: first node
+ * @b: second node
  */
-size_t heap_size(const binary_tree_t *root)
+void swap_values(heap_t *a, heap_t *b)
 {
-	if (!root)
-		return (0);
-	return (1 + heap_size(root->left) + heap_size(root->right));
+    int tmp = a->n;
+    a->n = b->n;
+    b->n = tmp;
 }
 
 /**
- * heap_get_last - gets pointer to last node by index
- * @root: pointer to root
- * @index: index of node to find (1-based)
- *
- * Return: pointer to node or NULL
- */
-heap_t *heap_get_last(heap_t *root, size_t index)
-{
-	return (binary_tree_get_node_by_index(root, index));
-}
-
-/**
- * swap_values - swaps 2 integers
- * @a: first int
- * @b: second int
- */
-void swap_values(int *a, int *b)
-{
-	int tmp = *a;
-
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * heapify_down - restores heap property going downward
+ * heapify_down - restores heap property downward
  * @root: pointer to root
  */
 void heapify_down(heap_t *root)
 {
-	heap_t *largest;
-	int swapped;
+    heap_t *largest = root;
 
-	do {
-		swapped = 0;
-		largest = root;
+    if (root->left && root->left->n > largest->n)
+        largest = root->left;
+    if (root->right && root->right->n > largest->n)
+        largest = root->right;
 
-		if (root->left && root->left->n > largest->n)
-			largest = root->left;
-		if (root->right && root->right->n > largest->n)
-			largest = root->right;
-
-		if (largest != root)
-		{
-			swap_values(&root->n, &largest->n);
-			root = largest;
-			swapped = 1;
-		}
-	} while (swapped);
+    if (largest != root)
+    {
+        swap_values(root, largest);
+        heapify_down(largest);
+    }
 }
 
 /**
- * heap_extract - extracts the root value of a Max Binary Heap
- * @root: pointer to heap root pointer
- *
- * Return: value of removed node, or 0 on failure
+ * heap_extract - extracts max value from Max Binary Heap
+ * @root: double pointer to heap root
+ * Return: value of removed node or 0 if fail
  */
 int heap_extract(heap_t **root)
 {
-	int value;
-	size_t size;
-	heap_t *last, *parent;
+    heap_t *last, *node;
+    int value;
 
-	if (!root || !*root)
-		return (0);
+    if (!root || !*root)
+        return (0);
 
-	value = (*root)->n;
-	size = heap_size(*root);
+    node = *root;
+    value = node->n;
 
-	if (size == 1)
-	{
-		free(*root);
-		*root = NULL;
-		return (value);
-	}
+    last = heap_get_last(*root);
 
-	last = heap_get_last(*root, size);
-	(*root)->n = last->n;
+    if (last == *root)
+    {
+        free(*root);
+        *root = NULL;
+        return (value);
+    }
 
-	parent = last->parent;
-	if (parent->right == last)
-		parent->right = NULL;
-	else
-		parent->left = NULL;
+    (*root)->n = last->n;
 
-	free(last);
-	heapify_down(*root);
+    if (last->parent->right == last)
+        last->parent->right = NULL;
+    else
+        last->parent->left = NULL;
 
-	return (value);
+    free(last);
+    heapify_down(*root);
+
+    return (value);
 }
