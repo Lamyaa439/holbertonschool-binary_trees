fe87846c3b4ac195fb73f72b4a72370ecac8fc46
lina_aziz <lenaexo0@gmail.com>
2025-10-30 12:22:49 +0300

    Update heap_get_last.c

--- heap_get_last.c
+++ heap_get_last.c
@@ -1,13 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * heap_get_last - gets pointer to last node in heap by index
- * @root: pointer to root
- * @index: index (1-based)
- *
- * Return: pointer to node or NULL
+ * heap_get_last - gets the last node in level-order of a heap
+ * @root: pointer to root of heap
+ * Return: pointer to last node, or NULL
  */
-heap_t *heap_get_last(heap_t *root, size_t index)
+binary_tree_t *heap_get_last(binary_tree_t *root)
 {
-    return (binary_tree_get_node_by_index(root, index));
+    binary_tree_t *queue[1024];
+    size_t head = 0, tail = 0;
+
+    if (!root)
+        return (NULL);
+
+    queue[tail++] = root;
+
+    while (head < tail)
+    {
+        root = queue[head++];
+
+        if (root->left)
+            queue[tail++] = root->left;
+        if (root->right)
+            queue[tail++] = root->right;
+    }
+
+    return (root);
 }
